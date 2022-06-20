0ab11b3de1d6eeb7ce57d79dba89b8da7265540c
Polina Blagoobrazova <polinoz@me.com>
2022-06-20 19:58:52 +0300

    Finish the first version of the layout

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //make 16x16 grid of square divs
 
-const squaresBox = document.getElementById("squaresBox");
+const screen = document.getElementById("screen");
 
-function makeRows(rows, cols) {
-  squaresBox.style.setProperty('--grid-rows', rows);
-  squaresBox.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    const square = document.createElement("div");
-    square.innerText = (c + 1);
-    squaresBox.appendChild(square).className = "grid-item";
-  };
-};
+//function makeRows(rows, cols) {
+//screen.style.setProperty('--grid-rows', rows);
+ // screen.style.setProperty('--grid-cols', cols);
+//  for (c = 0; c < (rows * cols); c++) {
+ //   const square = document.createElement("div");
+ //   square.innerText = (c + 1);
+  //  screen.appendChild(square).className = "grid-item";
+ // };
+//};
 
-makeRows(16, 16);
+//makeRows(16, 16);
